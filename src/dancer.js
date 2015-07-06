66a9bf8465be12e2ceb8b2823cfe15c54ec3c086
dierat <diedra.rater@gmail.com>
2015-07-06 16:30:20 -0700

    fixes step to be called by this and also pass this back to it using bind

--- src/dancer.js
+++ src/dancer.js
@@ -4,14 +4,14 @@ var Dancer = function(top, left, timeBetweenSteps){
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
 
-  /*
-  dancer.step = function(){
+  
+  this.step = function(){
     // the basic dancer doesn't do anything interesting at all on each step,
     // it just schedules the next step
-    setTimeout(dancer.step, timeBetweenSteps);
+    setTimeout(this.step.bind(this), timeBetweenSteps);
   };
-  dancer.step();
-  */
+  this.step();
+  
 
   this.setPosition = function(top, left){
     // Use css top and left properties to position our <span> tag
