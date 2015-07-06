8948f9b4d069f2d1fba92708c323a9924b4feb48
dierat <diedra.rater@gmail.com>
2015-07-06 16:02:31 -0700

    commenting out step functionality for now

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -4,6 +4,7 @@ var BlinkyDancer = function(top, left, timeBetweenSteps){
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 
+  /*
   var oldStep = blinkyDancer.step;
 
   blinkyDancer.step = function(){
@@ -14,6 +15,7 @@ var BlinkyDancer = function(top, left, timeBetweenSteps){
     // other effects you can use on a jQuery-wrapped html tag.
     blinkyDancer.$node.toggle();
   };
+  */
 
   return blinkyDancer;
 };
--- src/dancer.js
+++ src/dancer.js
@@ -6,13 +6,14 @@ var Dancer = function(top, left, timeBetweenSteps){
   // use jQuery to create an HTML <span> tag
   dancer.$node = $('<span class="dancer"></span>');
 
-
+  /*
   dancer.step = function(){
     // the basic dancer doesn't do anything interesting at all on each step,
     // it just schedules the next step
     setTimeout(dancer.step, timeBetweenSteps);
   };
   dancer.step();
+  */
 
   dancer.setPosition = function(top, left){
     // Use css top and left properties to position our <span> tag
