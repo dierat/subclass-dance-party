e9ce79e494852179839845d0a90a4b50a072bfed
dierat <diedra.rater@gmail.com>
2015-07-06 15:43:01 -0700

    removes 'make' from class constructor names, adds some tests for normal dancer, adds normal dancer button to dancefloor

---
+++ spec/dancerSpec.js
@@ -0,0 +1,29 @@
+describe("dancer", function() {
+
+  var dancer;
+  var timeBetweenSteps = 100;
+  var clock;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    dancer = Dancer(10, 20, timeBetweenSteps);
+  });
+
+  it("should have a jQuery $node object", function(){
+    expect(dancer.$node).to.be.an.instanceof(jQuery);
+  });
+
+  describe("dance", function(){
+    it("should call step at least once per second", function(){
+      sinon.spy(dancer, "step");
+      expect(dancer.step.callCount).to.be.equal(0);
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+      clock.tick(timeBetweenSteps);
+
+      expect(dancer.step.callCount).to.be.equal(1);
+
+      clock.tick(timeBetweenSteps);
+      expect(dancer.step.callCount).to.be.equal(2);
+    });
+  });
+});
--- src/dancer.js
+++ src/dancer.js
@@ -1,5 +1,5 @@
 // Creates and returns a new dancer object that can step
-var makeDancer = function(top, left, timeBetweenSteps){
+var Dancer = function(top, left, timeBetweenSteps){
 
   var dancer = {};
 
