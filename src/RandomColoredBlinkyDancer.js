cd3b19c5c29c7f1fd53fac4462bbab7bca0d527f
dierat <diedra.rater@gmail.com>
2015-07-06 20:01:11 -0700

    adds a dancer that is a random color and blinks

---
+++ src/RandomColoredBlinkyDancer.js
@@ -0,0 +1,17 @@
+var RandomColoredBlinkyDancer = function(top, left, delay){
+  RandomColoredDancer.call(this, top, left, delay);
+
+  this.oldStep = this.step;
+
+  this.step = function(){
+    // call the old version of step at the beginning of any call to this new version of step
+    this.oldStep();
+    // toggle() is a jQuery method to show/hide the <span> tag.
+    // See http://api.jquery.com/category/effects/ for this and
+    // other effects you can use on a jQuery-wrapped html tag.
+    this.$node.toggle();
+  };
+}
+
+RandomColoredBlinkyDancer.prototype = Object.create(RandomColoredDancer.prototype);
+RandomColoredBlinkyDancer.prototype.constructor = RandomColoredBlinkyDancer;
