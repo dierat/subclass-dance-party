5677955ad962d74ed1c0051e7721e09951a54253
dierat <diedra.rater@gmail.com>
2015-07-07 16:42:14 -0700

    moves methods outside of constructor function and onto the prototype and sets the constructor to point back to this

--- src/randomColoredDancer.js
+++ src/randomColoredDancer.js
@@ -1,18 +1,18 @@
 var RandomColoredDancer = function(top, left, timeBetweenSteps){
   Dancer.call(this, top, left, timeBetweenSteps);
 
-  this.setColor = function(){
-    // Use css to set color of dancer. See http://api.jquery.com/css/
-    var color = ranColor();
-
-    var styleSettings = {
-      "border-color": color
-    };
-    this.$node.css(styleSettings);
-  };
-
   this.setColor();
-
 };
 
 RandomColoredDancer.prototype = Object.create(Dancer.prototype);
+RandomColoredDancer.prototype.constructor = RandomColoredDancer;
+
+RandomColoredDancer.prototype.setColor = function(){
+  var color = ranColor();
+
+  var styleSettings = {
+    "border-color": color
+  };
+  
+  this.$node.css(styleSettings);
+};
