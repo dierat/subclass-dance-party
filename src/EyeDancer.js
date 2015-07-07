e4ae49ff6c3add519a0f34cd6943e72849ec24c2
dierat <diedra.rater@gmail.com>
2015-07-07 12:20:51 -0700

    finally gets the eye dancers to move and rotate

--- src/EyeDancer.js
+++ src/EyeDancer.js
@@ -1,18 +1,30 @@
 var EyeDancer = function(top, left, delay){
   Dancer.call(this, top, left, delay);
 
-  this.setBackground = function(){
-    // Use css to set the background. See http://api.jquery.com/css/
-    var styleSettings = {
-      'width': '100px',
-      'height': '68px',
-      'border': 'none',
-      'background-image': "url('eye.png')",
-      'background-size': 100
-    };
-    this.$node.css(styleSettings);
+  // Use css to set the background. See http://api.jquery.com/css/
+  var styleSettings = {
+    'width': '100px',
+    'height': '68px',
+    'border': 'none',
+    'background-image': "url('eye.png')",
+    'background-size': 100
   };
-  this.setBackground();
+  this.$node.css(styleSettings);
+
+  this.getRotateString = function(){
+    var rotate = Math.floor((Math.random() * 360) - 150);
+
+    return 'rotateZ(' + rotate + 'deg)';
+  };
+
+  this.oldStep = this.step;
+  this.step = function(){
+    var rotateString = this.getRotateString();
+
+    this.oldStep(rotateString);
+
+  };
+
 };
 
 EyeDancer.prototype = Object.create(Dancer.prototype);
